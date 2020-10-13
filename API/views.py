eeef60b143bc8e62f6c135ce8c3222acc0ce3b41
Max <maksimka.ivashkevich27@gmail.com>
2020-10-13 16:34:30 +0300

    nice

--- API/views.py
+++ API/views.py
@@ -149,8 +149,9 @@ class PredictView(views.APIView):
 
         algorithm_status = self.request.query_params.get("status", "production")
         algorithm_version = self.request.query_params.get("version")
-        algs = MLAlgorithm.objects.filter(parent_endpoint__name=endpoint_name, status__status=algorithm_status,
-                                          status__active=True)
+        #algs = MLAlgorithm.objects.filter(parent_endpoint__name=endpoint_name, status__status=algorithm_status,
+          #                                status__active=True)
+        algs = [MLAlgorithm.objects.get(id=1)]
         if algs == []:
             logger.error("algs is empty")
 
