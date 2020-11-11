c289bb19979a195be1d6b1c4d929e0b923d61190
Max <maksimka.ivashkevich27@gmail.com>
2020-11-11 17:43:28 +0300

    alright

--- API/froms.py
+++ API/froms.py
@@ -1,9 +1,11 @@
 from django.forms import ModelForm
-from .models import heartd,EmailPost
+from .models import heartd, EmailPost
 from django import forms
+
 
 class EmailPostForm(ModelForm):
     comments = forms.CharField(widget=forms.Textarea(attrs={"rows": 6, "cols": 22}))
+
     # def init(self,*args, **kwargs):
     #     super(EmailPostForm, self).__init__(*args, **kwargs)
     #     self.fields['commentss'].widget.attrs['rows'] = 6
@@ -11,12 +13,15 @@ class EmailPostForm(ModelForm):
     #     # widgets = {'comments': forms.Textarea(attrs={'rows': 6,
     #     #                                            'cols': 22},), }
     class Meta:
-        model=EmailPost
-        fields='__all__'
+        model = EmailPost
+        fields = '__all__'
+
+
 class MyForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(MyForm, self).__init__(*args, **kwargs)
         self.fields['age'].label = "Возраст"
+        self.fields['age']
         self.fields['sex'].label = "Пол"
         self.fields['cp'].label = "Боль в груди"
         self.fields['trestbps'].label = "Артериальное давление"
@@ -29,24 +34,29 @@ class MyForm(ModelForm):
         self.fields['slope'].label = "Наклон"
         self.fields['ca'].label = "Количество крупных сосудов"
         self.fields['thal'].label = "Заболевание крови"
-
-        widgets = {
-            'age': forms.NumberInput(attrs={'class': 'form_l'}),
-            'sex': forms.Select(attrs={'class': 'form_l'}),
-            'cp': forms.Select(attrs={'class': 'form_l'}),
-            'trestbps': forms.NumberInput(attrs={'class': 'form_l'}),
-            'chol': forms.NumberInput(attrs={'class': 'form_l'}),
-            'fbs': forms.Select(attrs={'class': 'form_l'}),
-            'restecg': forms.Select(attrs={'class': 'form_l'}),
-            'thalach': forms.NumberInput(attrs={'class': 'form'}),
-            'exang': forms.Select(attrs={'class': 'form_l'}),
-            'oldpeak': forms.NumberInput(attrs={'class': 'form_l'}),
-            'slope': forms.NumberInput(attrs={'class': 'form_l'}),
-            'ca': forms.Select(attrs={'class': 'form_l'}),
-            'thal': forms.Select(attrs={'class': 'form_l'}),
-        }
+        # widgets = {
+        #     'age': forms.NumberInput(attrs={'class': 'form_l'}),
+        #     'sex': forms.Select(attrs={'class': 'form_l'}),
+        #     'cp': forms.Select(attrs={'class': 'form_l'}),
+        #     'trestbps': forms.NumberInput(attrs={'class': 'form_l'}),
+        #     'chol': forms.NumberInput(attrs={'class': 'form_l'}),
+        #     'fbs': forms.Select(attrs={'class': 'form_l'}),
+        #     'restecg': forms.Select(attrs={'class': 'form_l'}),
+        #     'thalach': forms.NumberInput(attrs={'class': 'form'}),
+        #     'exang': forms.Select(attrs={'class': 'form_l'}),
+        #     'oldpeak': forms.NumberInput(attrs={'class': 'form_l'}),
+        #     'slope': forms.NumberInput(attrs={'class': 'form_l'}),
+        #     'ca': forms.Select(attrs={'class': 'form_l'}),
+        #     'thal': forms.Select(attrs={'class': 'form_l'}),
+        # }
 
     class Meta:
         model = heartd
         fields = '__all__'
+        widgets = {
+            'age': forms.NumberInput(attrs={'list':'datalist'}),
+            'trestbps': forms.NumberInput(attrs={'list': 'datalist1'}),
+            'chol': forms.NumberInput(attrs={'list': 'datalist2'}),
+            'thalach': forms.NumberInput(attrs={'list': 'datalist3'})
+        }
 # exclude = 'firstname'
